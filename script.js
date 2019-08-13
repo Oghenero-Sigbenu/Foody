9435a39371dff966391c84826f3e63e966511b9a
Oghenero-Sigbenu <sigbenuoghenero@gmail.com>
2019-08-12 22:19:22 -0700

    made some changes

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ let details = document.getElementById("nav-menu")
 //on clicking on hamb-menu it should change it class 
 ham.addEventListener("click", () => {
    ham.classList.toggle("down");
-    console.log("love")
-    if(details.style.display === 'block'){
-        details.style.display = 'none';
-    }else{
-        details.style.display = 'block';
+console.log("love")
+if(details.style.display === 'flex'){
+    details.style.display = 'none';
+}else{
+    details.style.display = 'flex';
+    details.className += "responsive"
     };
 
  })
